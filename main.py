71ebfb7339a138fef4a0a35bf3b599acfce699b8
ravenparker <ravenparker158@gmail.com>
2024-02-19 23:45:26 -0500

    added hello and ping commands

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ client = commands.Bot(command_prefix="!", intents= discord.Intents.all())
 
 @client.event
 async def on_ready():
+    await client.wait_until_ready()
+    await client.change_presence(activity=discord.activity.Game(name="with Python"), 
+                                status=discord.Status.idle)
     print(f'{client.user.name} is now online')
 
-client.run("MTIwOTI0MTg4OTcxNTI1NzM5NA.Gdm0pe.qQWI9d6wvFu3mxwqhnui_qS1MMZAP_N8HZr4jA")
+@client.command(name = "hello", description = "Say hi!")
+async def hello(ctx):
+    await ctx.send("hey there!")
+
+
+@client.command(name = "ping", description = "Latency check")
+async def ping(ctx):
+    bot_latency = round(client.latency*1000)
+    await ctx.send(f"Pong! {bot_latency}ms")
+
+client.run('MTIwOTI0MTg4OTcxNTI1NzM5NA.GHXewE.ms3dx9UaF7s7PJb6Qj-STs_yCt2qBB6u17M0-o')
